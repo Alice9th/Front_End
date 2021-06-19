6bac489d7bf11b223c553c5d8cb39ec14d51a707
Alice9th <monicazapata.delvalle@gmail.com>
2021-06-19 17:26:24 -0500

    Updates in Front End using JS

--- FE2/playgroundFE2.js
+++ FE2/playgroundFE2.js
@@ -10,6 +10,24 @@
   
   });
 
+  //Ejercicio 2:
+
+  window.addEventListener('load', function() {
+
+    // escribí tu código aquí
+    let mod = document.querySelector('h1');
+    mod.classList.remove("titulo")
+  
+  });
+
+  //Ejercicio 3:
+  window.addEventListener('load', function() {
+
+    // escribí tu código aquí
+    let titulo = document.querySelector("h1");
+    titulo.classList.toggle('titulo-tuneado')
+  
+  });
 
 
 //Eventos:
@@ -67,14 +85,49 @@ window.addEventListener('load', function() {
   
     for(let link of links) {
       link.addEventListener("mouseout", function(){
-        this.classList.toggle() //Si el link no tiene la clase "dark-mode", asignársela y si la tiene, quitársela
+        this.classList.toggle("dark-mode") //Si el link no tiene la clase "dark-mode", asignársela y si la tiene, quitársela
       })
     }
   
   });
 
-  //Eventos de teclado:
+//Eventos de teclado:
 
+// Ejercicio 1:
 
+window.addEventListener('load', function() {
+
+    // escribí tu codigo acá
+    let userName = document.querySelector('#userName')
+   
+      userName.addEventListener('keydown', function(event){
+          if (event.key == '#'){
+          alert("Está prohibido el uso de #hashtags!")
+          }
+      })
+  });
   
 
+//Ejercicio 2:
+
+window.addEventListener('load', function() {
+
+    let input = document.querySelector('#input'); 
+    
+    input.addEventListener('keyup', (event) => {
+        let presionar = event.key;
+        alert("Se presionó la tecla: " + presionar)
+    })
+
+});
+
+//Ejercicio 3:
+window.addEventListener('load', function(){
+
+    let direccion = document.querySelector('#address')
+
+    direccion.addEventListener('keypress', function(event){
+         alert("Se presionó la tecla: "+ event.key);
+    })
+
+})
